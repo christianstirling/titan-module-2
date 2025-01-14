a5444bb3f3c2918bf81814e9e2cc94d62dc5a3ed
Christian Stirling <77996757+christianstirling@users.noreply.github.com>
2025-01-14 13:11:05 -0600

    Add template file
    Added a template.js file to be used to create new input objects in the index.js file.

---
+++ template.js
@@ -0,0 +1,68 @@
+
+
+const templateArray = [
+    {
+        // This is the template for a force type that does not have any "attributes"
+        name: "Task Name",
+        handleType: "Key ? Door Knob ? Ridged Knob ? Tap ? Wing Nut",
+        modifiers: [],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right ? Left"
+    },
+    {
+        name: "Task Name",
+        handleType: "L-Shaped",
+        modifiers: ["Counter-clockwise ? Clockwise"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right ? Left"
+    },
+    {
+        name: "Task Name",
+        handleType: "Jar Lid",
+        modifiers: ["Knurled ? Smooth", "1.8 inch diameter ? 2.6 inch diameter ? 3.3 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right ? Left"
+    },
+    {
+        name: "Task Name",
+        handleType: "Round Knob",
+        modifiers: ["Knurled ? Smooth", "0.125 inch diameter ? 0.25 inch diameter ? 0.5 inch diameter ? 0.75 inch diameter ? 1.0 inch diameter ? 2.0 inch diameter ? 3.0 inch diameter ? 4.0 inch diameter ? 5.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right ? Left"
+    },
+    {
+        name: "Task Name",
+        handleType: "Regular Screwdriver ? Pistol Grip Screwdriver",
+        modifiers: ["Counter-clockwise ? Clockwise", "Palm-down (60 degrees Pronated) ? Hand-shake (Neutral) ? Palm-up (60 degrees Supinated)"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right ? Left"
+    },
+    {
+        name: "Task Name",
+        handleType: "T-Handle",
+        modifiers: ["Elbow Angle 180 degrees ? Elbow Angle 135 degrees ? Elbow Angle 90 degrees ? Elbow Angle 45 degrees", "Counter-clockwise ? Clockwise", "Palm-down (75% Range of Motion) ? Hand-shake (Neutral) ? Palm-up (75% Range of Motion)"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right ? Left"
+    },
+    {
+        name: "Task Name",
+        handleType: "Cylindrical Handle",
+        modifiers: ["Smooth ? Knurled", "Outward ? Inward", "1.8 inch diameter ? 2.3 inch diameter ? 3.3 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right ? Left"
+    }
+]
