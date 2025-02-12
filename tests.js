c87f0d6d7862347845f1532f05d9327d67009bbc
Christian Stirling <77996757+christianstirling@users.noreply.github.com>
2025-02-12 13:46:24 -0600

    Create test input sets

---
+++ tests.js
@@ -0,0 +1,468 @@
+/*  Test input sets for module 2 calculator.
+*/
+
+const input = []
+
+/*  Test set 1 - All of the handle types that have no modifiers.
+
+    This will include (1)Key, (2)Door Knob, (4)Ridged Knob, (5)Tap, (6)Wing Nut
+*/  
+
+input = [
+    {
+        name: "Task 1a",
+        handleType: "Key",
+        modifiers: [],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 1b",
+        handleType: "Door Knob",
+        modifiers: [],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 1c",
+        handleType: "Ridged Knob",
+        modifiers: [],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 1d",
+        handleType: "Tap",
+        modifiers: [],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 1e",
+        handleType: "Wing Nut",
+        modifiers: [],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    }
+]
+
+/*  Test set 2 - Handle type: (3)L-Shaped 
+*/
+
+input = [
+    {
+        name: "Task 2a",
+        handleType: "L-Shaped",
+        modifiers: ["Counter-clockwise"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 2b",
+        handleType: "L-Shaped",
+        modifiers: ["Clockwise"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 2c",
+        handleType: "L-Shaped",
+        modifiers: ["Clockwise"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Left"
+    },
+    {
+        name: "Task 2d",
+        handleType: "L-Shaped",
+        modifiers: ["Counter-clockwise"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Left"
+    }
+]
+
+/*  Test set 3 - Handle type: (7)Jar Lid 
+*/
+
+input = [
+    {
+        name: "Task 3a",
+        handleType: "Jar Lid",
+        modifiers: ["Knurled", "1.8 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 3b",
+        handleType: "Jar Lid",
+        modifiers: ["Knurled", "2.6 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 3c",
+        handleType: "Jar Lid",
+        modifiers: ["Knurled", "3.3 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 3d",
+        handleType: "Jar Lid",
+        modifiers: ["Smooth", "1.8 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 3e",
+        handleType: "Jar Lid",
+        modifiers: ["Smooth", "2.6 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 3f",
+        handleType: "Jar Lid",
+        modifiers: ["Smooth", "3.3 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    }
+]
+
+/*  Test set 4 - Handle type: (8)Round Knob  - Texture: Knurled
+*/
+
+input = [
+    {
+        name: "Task 4a",
+        handleType: "Round Knob",
+        modifiers: ["Knurled", "0.125 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 4b",
+        handleType: "Round Knob",
+        modifiers: ["Knurled", "0.25 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 4c",
+        handleType: "Round Knob",
+        modifiers: ["Knurled", "0.5 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 4d",
+        handleType: "Round Knob",
+        modifiers: ["Knurled", "0.75 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 4e",
+        handleType: "Round Knob",
+        modifiers: ["Knurled", "1.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 4f",
+        handleType: "Round Knob",
+        modifiers: ["Knurled", "2.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 4g",
+        handleType: "Round Knob",
+        modifiers: ["Knurled", "3.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 4h",
+        handleType: "Round Knob",
+        modifiers: ["Knurled", "4.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 4i",
+        handleType: "Round Knob",
+        modifiers: ["Knurled", "5.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    }
+]
+
+/*  Test set 5 - Handle type: (8)Round Knob  - Texture: Smooth
+*/
+
+input = [
+    {
+        name: "Task 5a",
+        handleType: "Round Knob",
+        modifiers: ["Smooth", "0.125 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 5b",
+        handleType: "Round Knob",
+        modifiers: ["Smooth", "0.25 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 5c",
+        handleType: "Round Knob",
+        modifiers: ["Smooth", "0.5 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 5d",
+        handleType: "Round Knob",
+        modifiers: ["Smooth", "0.75 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 5e",
+        handleType: "Round Knob",
+        modifiers: ["Smooth", "1.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 5f",
+        handleType: "Round Knob",
+        modifiers: ["Smooth", "2.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 5g",
+        handleType: "Round Knob",
+        modifiers: ["Smooth", "3.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 5h",
+        handleType: "Round Knob",
+        modifiers: ["Smooth", "4.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 5i",
+        handleType: "Round Knob",
+        modifiers: ["Smooth", "5.0 inch diameter"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    }
+]
+
+/*  Test set 6 - Handle type: (9)Regular Screwdriver
+*/
+
+input = [
+    {
+        name: "Task 6a",
+        handleType: "Regular Screwdriver",
+        modifiers: ["Counter-clockwise", "Palm-down (60 degrees Pronated)"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 6b",
+        handleType: "Regular Screwdriver",
+        modifiers: ["Counter-clockwise", "Hand-shake (Neutral)"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 6c",
+        handleType: "Regular Screwdriver",
+        modifiers: ["Counter-clockwise", "Palm-up (60 degrees Supinated)"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+
+    {
+        name: "Task 6d",
+        handleType: "Regular Screwdriver",
+        modifiers: ["Clockwise", "Palm-down (60 degrees Pronated)"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 6e",
+        handleType: "Regular Screwdriver",
+        modifiers: ["Clockwise", "Hand-shake (Neutral)"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    },
+    {
+        name: "Task 6f",
+        handleType: "Regular Screwdriver",
+        modifiers: ["Clockwise", "Palm-up (60 degrees Supinated)"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Right"
+    }
+]
+
+/*  Test set 7 - Handle type: (9)Regular Screwdriver - Hand: Left
+*/
+
+input = [
+    {
+        name: "Task 6a",
+        handleType: "Regular Screwdriver",
+        modifiers: ["Clockwise", "Palm-down (60 degrees Pronated) ? Hand-shake (Neutral) ? Palm-up (60 degrees Supinated)"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Left"
+    },
+
+    {
+        name: "Task 6a",
+        handleType: "Regular Screwdriver",
+        modifiers: ["Counter-clockwise", "Palm-down (60 degrees Pronated) ? Hand-shake (Neutral) ? Palm-up (60 degrees Supinated)"],
+        forceCount: 1,
+        forceMagnitude: 1,
+        forceDuration: 1,
+        hand: "Left"
+    }
+]
+
+
+/*  Test set 8 - Handle type: (10)Pistol Grip Screwdriver - Hand: Right
+*/
+
+/*  Test set 9 - Handle type: (10)Pistol Grip Screwdriver - Hand: Left
+*/
+
+
+
+
+
+
+
+/*  Test set 8 - Handle type: (11)T-Handle - Hand: Right - Elbow posture: 180 degrees
+*/
+
+/*  Test set 9 - Handle type: (11)T-Handle - Hand: Right - Elbow posture: 135 degrees
+*/
+
+/*  Test set 10 - Handle type: (11)T-Handle - Hand: Right - Elbow posture: 90 degrees
+*/
+
+/*  Test set 11 - Handle type: (11)T-Handle - Hand: Right - Elbow posture: 45 degrees
+*/
+
+/*  Test set 12 - Handle type: (11)T-Handle - Hand: Left - Elbow posture: 180 degrees
+*/
+
+/*  Test set 13 - Handle type: (11)T-Handle - Hand: Left - Elbow posture: 135 degrees
+*/
+
+/*  Test set 14 - Handle type: (11)T-Handle - Hand: Left - Elbow posture: 90 degrees
+*/
+
+/*  Test set 15 - Handle type: (11)T-Handle - Hand: Left - Elbow posture: 45 degrees
+*/
+
+/*  Test set 16 - Handle type: (12)Cylindrical Handle - Hand: Right - Texture: Smooth
+*/
+
+/*  Test set 17 - Handle type: (12)Cylindrical Handle - Hand: Right - Texture: Knurled
+*/
+
+/*  Test set 18 - Handle type: (12)Cylindrical Handle - Hand: Left - Texture: Smooth
+*/
+
+/*  Test set 19 - Handle type: (12)Cylindrical Handle - Hand: Left - Texture: Knurled
+*/
